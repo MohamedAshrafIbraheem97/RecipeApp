9a265209c071aac61dbc97b76ab427504413c4e4
Mohamed Ashraf Ibraheem <mohamedashrafibraheem@gmail.com>
2024-02-19 17:16:20 +0200

    recipe details design

--- src/app/recipe/components/recipe-details/recipe-details.component.ts
+++ src/app/recipe/components/recipe-details/recipe-details.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.component.html',
-  styleUrls: ['./recipe-details.component.scss']
+  styleUrls: ['./recipe-details.component.scss'],
 })
 export class RecipeDetailsComponent {
-
+  isFavorite: boolean = false;
+  addToFavorites() {}
 }
